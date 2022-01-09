5b3b9dd90974871f3ddd7236be365a4c62fed761
Nicolas Knaian <nickknaian@gmail.com>
2022-01-09 10:05:03 -0500

    Remove 'about' page

--- musicorg/main/handlers.py
+++ musicorg/main/handlers.py
@@ -1,10 +1,5 @@
 from flask import render_template, redirect, url_for, flash
 
-from musicorg.spotify.item.spotify_music import SpotifyAlbum
-from musicorg.spotify import spotify_user
-
-from musicorg import cache, spotify_iface
-
 from . import bp
 
 
@@ -12,8 +7,3 @@ from . import bp
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('main/index.html')
-
-
-@bp.route('/about', methods=['GET'])
-def about():
-    return render_template('main/about.html')
