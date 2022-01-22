bba49dc4d0941b17187bc43f3b3f5089f6c6910e
Nicolas Knaian <nickknaian@gmail.com>
2022-01-22 16:55:51 -0500

    some more cleanup

--- albumcollections/config.py
+++ albumcollections/config.py
@@ -5,14 +5,9 @@ class Config(object):
     """This class is used to configure the flask app using
     default settings and environment variable settings
     """
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     CACHE_TYPE = 'FileSystemCache'
     CACHE_DIR = './.flask_caching/'
     CACHE_DEFAULT_TIMEOUT = 300
-
-    SCHEDULER_API_ENABLED = True
 
     SESSION_TYPE = 'filesystem'
     SESSION_FILE_DIR = './.flask_session/'
