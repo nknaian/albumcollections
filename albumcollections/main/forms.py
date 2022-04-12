2b0339474d8a043f366cde23af4e1abc9411d71a
Nicolas Knaian <nickknaian@gmail.com>
2022-04-11 22:24:47 -0400

    Add way of removing collections as well

--- albumcollections/main/forms.py
+++ albumcollections/main/forms.py
@@ -4,5 +4,15 @@ from wtforms.fields.choices import SelectMultipleField
 
 
 class AddCollectionsForm(FlaskForm):
-    available_playlists = SelectMultipleField('Available Playlists', choices=[])
+    playlists = SelectMultipleField('Available Playlists', choices=[])
     submit_new_collections = SubmitField('Add')
+
+
+class RemoveCollectionsForm(FlaskForm):
+    collections = SelectMultipleField(
+        'Collections',
+        choices=[],
+        description="Removes from your album collections account - this will"
+                    " NOT remove the underlying spotify playlist!"
+    )
+    submit_collection_removal = SubmitField('Remove')
--- albumcollections/main/handlers.py
+++ albumcollections/main/handlers.py
@@ -12,7 +12,7 @@ from albumcollections.models import Collection
 
 from albumcollections import db
 
-from .forms import AddCollectionsForm
+from .forms import AddCollectionsForm, RemoveCollectionsForm
 from . import bp
 
 
@@ -24,7 +24,9 @@ def index():
     # If user is logged in then process collection addition and removal forms
     # and load their collections
     if is_user_logged_in():
+        # Initialize forms
         add_collections_form = AddCollectionsForm()
+        remove_collections_form = RemoveCollectionsForm()
 
         # Get the user's playlists
         try:
@@ -35,27 +37,43 @@ def index():
         # Load the user's collections
         user_collections = _load_collections()
 
-        # Add the playlists to the form
-        add_collections_form.available_playlists.choices.extend(
+        # Add the playlist names to the add_collections_form
+        add_collections_form.playlists.choices.extend(
             _get_available_playlist_names(user_playlists, user_collections)
         )
 
-        # Handle form submission
-        if add_collections_form.validate_on_submit():
+        # Handle add_collections_form submission
+        if add_collections_form.submit_new_collections.data and add_collections_form.validate():
             _add_collections(add_collections_form, user_playlists)
             return redirect(url_for('main.index'))
         elif add_collections_form.errors:
-            flash("Failed to add collections", "error")
+            flash("Failed to add collections", "danger")
+
+        # Add the collection names to the remove_collections_form
+        remove_collections_form.collections.choices.extend(
+            [collection.name for collection in user_collections]
+        )
+
+        # Handle remove_collections_form submission
+        if remove_collections_form.submit_collection_removal.data and remove_collections_form.validate():
+            _remove_collections(remove_collections_form, user_collections)
+            return redirect(url_for('main.index'))
+        elif remove_collections_form.errors:
+            print("rerrors?: ", remove_collections_form.errors)
+            flash("Failed to remove collections", "danger")
+
 
     # Otherwise load nothing
     else:
-        add_collections_form = None
         user_collections = None
+        add_collections_form = None
+        remove_collections_form = None
 
     return render_template(
         'main/index.html',
         user_collections=user_collections,
-        add_collections_form=add_collections_form
+        add_collections_form=add_collections_form,
+        remove_collections_form=remove_collections_form
     )
 
 
@@ -81,14 +99,14 @@ def _add_collections(add_collections_form: AddCollectionsForm, user_playlists: L
     chosen_user_playlists = [
         user_playlist
         for user_playlist in user_playlists
-        if user_playlist.name in add_collections_form.available_playlists.data
+        if user_playlist.name in add_collections_form.playlists.data
     ]
 
     # Add new collections to the db
     for user_playlist in chosen_user_playlists:
         try:
             if Collection.query.filter_by(playlist_id=user_playlist.id, user_id=get_user_id()).first():
-                raise Exception(f"'{user_playlist.name}' already present in your account")
+                raise Exception(f"'{user_playlist.name}' already imported as a collection")
             else:
                 db.session.add(Collection(playlist_id=user_playlist.id, user_id=get_user_id()))
                 db.session.commit()
@@ -96,7 +114,31 @@ def _add_collections(add_collections_form: AddCollectionsForm, user_playlists: L
             raise albumcollectionsError(f"Failed to add collections - {e}", url_for('main.index'))
 
 
+def _remove_collections(remove_collections_form: RemoveCollectionsForm, user_collections: List[SpotifyCollection]):
+    # Get user's chosen collections to remove from the form
+    collections_to_delete = [
+        user_collection
+        for user_collection in user_collections
+        if user_collection.name in remove_collections_form.collections.data
+    ]
+
+    print("collections to delete: ", collections_to_delete)
+
+    for collection in collections_to_delete:
+        try:
+            collection = Collection.query.filter_by(playlist_id=collection.id, user_id=get_user_id()).first()
+            if collection is None:
+                raise Exception(f"'{collection.name}' not found")
+            else:
+                db.session.delete(collection)
+                db.session.commit()
+        except Exception as e:
+            raise albumcollectionsError(f"Failed to remove collections - {e}", url_for('main.index'))
+
+
 def _load_collections() -> List[SpotifyCollection]:
+    # TODO: This function is taking too long. Should make it so this utilizes the 'user_playlists' object
+    # that we already retrieved instead of making more 
     return [
         spotify_iface.Spotify().get_collection(collection_entry.playlist_id, load_albums=False)
         for collection_entry in Collection.query.filter_by(user_id=get_user_id())
